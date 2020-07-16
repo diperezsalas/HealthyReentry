8de366fe8933bf169b630db28597211fa15883e6
Sergio García <sgarciademarina@dimaticasoftware.com>
2020-07-16 16:23:30 +0200

    404

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,17 +1,14 @@
 <template>
-  <div>
-    <md-list>
-      <md-icon class="md-size-5x far fa-eye-slash mx-auto mt-5"></md-icon>
-      <md-empty-state
-        md-label="404 Page not Found"
-        md-description="The page you are trying to access does not exist.">
+  <div class="text-center">
+    <div style="height: 80px"></div>
+
+    <h1 style="color:white;font-size: 45px;margin: 50px;">404 Page not Found</h1>
+    <h5 style="color:white;font-family: 'endurance-light';margin: 50px;">The page you are trying to access does not exist.</h5>
         <router-link :to="{ name: '/' }">
           <md-button class="md-primary md-raised">
              Go to home page
           </md-button>
         </router-link>
-      </md-empty-state>
-    </md-list>
   </div>
 </template>
 
