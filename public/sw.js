920035a021884cc9f347dcd731cb866bfccf17ce
Sergio García <sgarciademarina@dimaticasoftware.com>
2020-07-20 11:47:16 +0200

    router manager

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,33 @@
 // CACHE
 const staticCacheName = 'site-static-v1';
 const assets = [
-  // "",
-  // "",
-  "/logo-256.png"
+    "/logo-256.png"
 ];
 // install event
 self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log('caching shell assets');
+            cache.addAll(assets);
+        })
+    );
 });
 // activate event
 self.addEventListener('activate', evt => {
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
+    evt.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(keys
+                .filter(key => key !== staticCacheName)
+                .map(key => caches.delete(key))
+            );
+        })
+    );
 });
 // fetch event
 self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
-    })
-  );
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
 });
--- src/router/index.js
+++ src/router/index.js
@@ -13,141 +13,173 @@ import Admin from '@/views/Admin.vue';
 import OfficeAdmin from '@/views/Offices.vue';
 import UsersAdmin from '@/views/Users.vue';
 import NotFound from '@/views/404.vue';
+import { getInstance, useAuth0 } from "../auth/index";
 
 import store from '@/store/index.js';
 
 import { authGuard } from "@/auth/authGuard";
+import { loginWithRedirect } from "@/auth/index";
 
 Vue.use(VueRouter);
 
-export default new VueRouter({
-  mode: 'history',
-  base: '/',
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home,
-    //   // beforeEnter: (to, from, next) => {
-    //   //   // if (store.state.user) {
-    //   //   //   console.log("store.state.user.dateOfConsent", store.state.user.dateOfConsent);
-    //   //   //   if (!store.state.user.dateOfConsent) return next('/disclosure');
-    //   //   //   else return next('/menu');
-    //   //   // }
-    //   //   // return next();
-    //   // }
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   component: About
-    // },
-    // {
-    //   path: '/profile',
-    //   name: 'profile',
-    //   component: Profile,
-    //   // beforeEnter: authGuard
-    // },
-    // {
-    //   path: '/test',
-    //   name: 'test',
-    //   component: Test,
-    //   // beforeEnter: authGuard
-    // }
-    {
-      path: '/',
-      name: 'home',
-      component: Intro,
-      beforeEnter: authGuard,
-      meta: {
-        title: route => "Healthy Reentry: Welcome"
-      },
-      props: true
-    },
-    {
-      path: '/disclosure',
-      name: 'disclosure',
-      component: Disclosure,
-      beforeEnter: authGuard,
-      meta: {
-        title: route => "Healthy Reentry: Disclosure and Consent"
-      },
-      props: true
-    },
-    {
-      path: '/menu',
-      name: 'menu',
-      component: Menu,
-      beforeEnter: authGuard,
-      meta: {
-        title: route => "Healthy Reentry: Menu"
-      },
-      props: true
-    },
-    {
-      path: '/encounter/:scannedUser?',
-      name: 'encounter',
-      component: Encounter,
-      meta: {
-        title: route => "Healthy Reentry: Record Your Encounter"
-      },
-      beforeEnter: authGuard,
-      props: true
-    },
-    {
-      path: '/:id/status',
-      name: 'status',
-      component: Status,
-      meta: {
-        title: route => "Healthy Reentry: Record Your Status"
-      },
-      beforeEnter: authGuard,
-      props: true
-    },
-    {
-      path: '/:id/displayqr',
-      name: 'displayqr',
-      component: DisplayQR,
-      meta: {
-        title: route => "Healthy Reentry: View Your QR Code"
-      },
-      beforeEnter: authGuard,
-      props: true
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: Admin,
-      meta: {
-        title: route => "Healthy Reentry: Admin View"
-      },
-      beforeEnter: authGuard,
-      props: true
-    },
-    {
-      path: '/adminusers',
-      name: 'adminusers',
-      component: UsersAdmin,
-      meta: {
-        title: route => "Healthy Reentry: Users Admin View"
-      },
-      beforeEnter: authGuard,
-      props: true
-    },
-    {
-      path: '/officeadmin',
-      name: 'officeadmin',
-      component: OfficeAdmin,
-      meta: {
-        title: route => "Healthy Reentry: Office Admin View"
-      },
-      beforeEnter: authGuard,
-      props: true
-    },
-    {
-        path: '*',
-        name: 'NotFound',
-        component: NotFound
-    }
-  ]
+const router = new VueRouter({
+    mode: 'history',
+    base: '/',
+    routes: [
+
+        {
+            path: '/',
+            name: 'home',
+            component: Intro,
+            beforeEnter: authGuard,
+            meta: {
+                title: route => "Healthy Reentry: Welcome",
+                access: 'full'
+            },
+            props: true
+        },
+        {
+            path: '/disclosure',
+            name: 'disclosure',
+            component: Disclosure,
+            beforeEnter: authGuard,
+            meta: {
+                title: route => "Healthy Reentry: Disclosure and Consent",
+                access: 'user'
+            },
+            props: true
+        },
+        {
+            path: '/menu',
+            name: 'menu',
+            component: Menu,
+            beforeEnter: authGuard,
+            meta: {
+                title: route => "Healthy Reentry: Menu",
+                access: 'user'
+            },
+            props: true
+        },
+        {
+            path: '/encounter/:scannedUser?',
+            name: 'encounter',
+            component: Encounter,
+            meta: {
+                title: route => "Healthy Reentry: Record Your Encounter",
+                access: 'user'
+            },
+            beforeEnter: authGuard,
+            props: true
+        },
+        {
+            path: '/:id/status',
+            name: 'status',
+            component: Status,
+            meta: {
+                title: route => "Healthy Reentry: Record Your Status",
+                access: 'user'
+            },
+            beforeEnter: authGuard,
+            props: true
+        },
+        {
+            path: '/:id/displayqr',
+            name: 'displayqr',
+            component: DisplayQR,
+            meta: {
+                title: route => "Healthy Reentry: View Your QR Code",
+                access: 'user'
+            },
+            beforeEnter: authGuard,
+            props: true
+        },
+        {
+            path: '/admin',
+            name: 'admin',
+            component: Admin,
+            meta: {
+                title: route => "Healthy Reentry: Admin View",
+                access: 'officeAdmin'
+            },
+            beforeEnter: authGuard,
+            props: true
+        },
+        {
+            path: '/adminusers',
+            name: 'adminusers',
+            component: UsersAdmin,
+            meta: {
+                title: route => "Healthy Reentry: Users Admin View",
+                access: 'admin'
+            },
+            beforeEnter: authGuard,
+            props: true
+        },
+        {
+            path: '/officeadmin',
+            name: 'officeadmin',
+            component: OfficeAdmin,
+            meta: {
+                title: route => "Healthy Reentry: Office Admin View",
+                access: 'admin'
+            },
+            beforeEnter: authGuard,
+            props: true
+        },
+        {
+            path: '*',
+            name: 'NotFound',
+            component: NotFound
+        }
+    ]
 });
+
+router.beforeEach((to, from, next) => {
+
+    const authService = getInstance();
+    if (authService.isAuthenticated) {
+        if (!allowAccess(authService.userDB.permissions, to.meta.access)) {
+            authService.loginWithRedirect();
+        } else {
+            next();
+        }
+    } else {
+        setTimeout(() => {
+            if (authService.isAuthenticated) {
+                authService.$api.post('/api/user', authService.user).then(returnedUser => {
+                    const user = returnedUser.data;
+
+                    if (!allowAccess(user.permissions, to.meta.access)) {
+                        authService.loginWithRedirect();
+                    } else {
+                        next();
+                    }
+                });
+
+            } else if (to.meta.access !== 'full') {
+                authService.loginWithRedirect();
+
+            }
+            next();
+        }, 1000);
+    }
+});
+
+let allowAccess = (user, access) => {
+    let allow = true;
+    switch (access) {
+        case 'admin':
+            if (user.admin === false) {
+                allow = false;
+            }
+            break;
+        case 'officeAdmin':
+            if (user.admin === false && user.office_admin === false) {
+                allow = false;
+            }
+            break;
+    }
+    return allow;
+};
+
+export default router;
